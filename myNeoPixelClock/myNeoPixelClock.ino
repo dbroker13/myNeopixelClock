aad7f0ccb78a2d612db91c79bf1ef76262f255ee
dbroker13 <davidbroker2@gmail.com>
2016-08-07 10:57:33 -0400

    changes

--- myNeoPixelClock/myNeoPixelClock.ino
+++ myNeoPixelClock/myNeoPixelClock.ino
@@ -39,6 +39,8 @@ class Button {
     Button( int myPin = 2 ) {
       pin = myPin;
       pinMode( pin, INPUT_PULLUP );
+      currentVal = false;
+      lastVal = false;
     }
 
     boolean togglePush() {
@@ -80,9 +82,9 @@ class Button {
     }
 
   private:
-    int pin = 2;
-    boolean currentVal = false;
-    boolean lastVal = false;
+    int pin;
+    boolean currentVal;
+    boolean lastVal;
     
     boolean readButton() {
       lastVal = currentVal;
@@ -108,24 +110,18 @@ byte hourval, minuteval, secondval; // holds the time
 
 byte pixelColorRed, pixelColorGreen, pixelColorBlue; // holds color values
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 int selectMode = 0;
 
->>>>>>> origin/master
 Button button1 = Button( BUTTON1 );
 Button button2 = Button( BUTTON2 );
 
 
-=======
->>>>>>> parent of 436b84e... disable fade and add blink logic
 void setup () {
   //Serial.begin(9600);
   Serial.begin(57600);
   Serial.println("START: setup");
 
-  Wire.begin();        	// Begin I2C
+  Wire.begin();  // Begin I2C
   RTC.begin();   // begin clock
 
   // set pinmodes
@@ -171,45 +167,33 @@ void loop () {
   for (uint8_t i = 0; i < strip.numPixels(); i++) {
 
     if (tailBand( i, secondval, 15.0 )) {
-<<<<<<< HEAD
       // calculates a faded arc from low to maximum brightness
-<<<<<<< HEAD
       //pixelColorBlue = (i + 1) * (255 / (secondval + 1));
-=======
->>>>>>> origin/master
       if( selectMode = 1 && blink ) {
-        pixelColorBlue = 0
+        pixelColorBlue = 0;
       } else {
         pixelColorBlue = 255;
       }
-=======
       pixelColorBlue = (i + 1) * (255 / (secondval + 1));
       //pixelColorBlue = 255;
->>>>>>> parent of 436b84e... disable fade and add blink logic
     }
     else {
       pixelColorBlue = 0;
     }
 
     if (deadBand( i, minuteval, 0.9 )) {
-<<<<<<< HEAD
       pixelColorGreen = 255;
-=======
       pixelColorGreen = (i + 1) * (255 / (minuteval + 1));
       //pixelColorGreen = 255;
->>>>>>> parent of 436b84e... disable fade and add blink logic
     }
     else {
       pixelColorGreen = 0;
     }
 
     if (deadBand( i, hourval, 2.5 )) {
-<<<<<<< HEAD
       pixelColorRed = 255;
-=======
       pixelColorRed = (i + 1) * (255 / (hourval + 1));
       //pixelColorRed = 255;
->>>>>>> parent of 436b84e... disable fade and add blink logic
     }
     else {
       pixelColorRed = 0;
@@ -233,16 +217,10 @@ void loop () {
 
   // wait
   delay(100);
-<<<<<<< HEAD
   blink = ! blink;
   if( button1.togglePush() ) {
     selectMode = cycleMode( selectMode );
   }
-<<<<<<< HEAD
-=======
->>>>>>> parent of 436b84e... disable fade and add blink logic
-
-=======
   if( button2.togglePush() ) {
      if( selectMode == 1 ) {
         hourval   = Clock.hour() + 1;
@@ -253,11 +231,10 @@ void loop () {
      if( selectMode == 3 ) {
         secondval   = Clock.second() + 1;
      }
-     rtc.adjust(DateTime( yearval, monthval, dayval, hourval, minuteval, secondval );
+     RTC.adjust(DateTime( yearval, monthval, dayval, hourval, minuteval, secondval ) );
      if (hourval > 11) hourval -= 12;           // This clock is 12 hour, if 13-23, convert to 0-11
      hourval = (hourval * 60 + minuteval) / 12; //each red dot represent 24 minutes.
   }
->>>>>>> origin/master
 }
 
 // Select setup mode
